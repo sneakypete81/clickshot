db054ab4e29c47e18d36d380bdadfaf69eb66ae7
sneakypete81 <sneakypete81@gmail.com>
2019-11-24 19:57:40 +0000

    Mock out pynput.keyboard when running unit tests
    This allows it to run headless on TravisCI

--- test/conftest.py
+++ test/conftest.py
@@ -3,4 +3,5 @@ from unittest import mock
 
 # Mock out modules that require a graphical interface
 sys.modules["pynput.mouse"] = mock.Mock()
+sys.modules["pynput.keyboard"] = mock.Mock()
 sys.modules["mss.linux"] = mock.Mock()
