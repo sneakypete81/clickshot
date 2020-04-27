9be7e165ba6f7496e1c933143e1639c1fc719411
sneakypete81 <sneakypete81@gmail.com>
2020-04-27 21:38:03 +0100

    Simplify time.monotonic mocking

--- test/test_retry.py
+++ test/test_retry.py
@@ -12,6 +12,23 @@ class AbortedTestError(Exception):
     pass
 
 
+class Timer:
+    def __init__(self):
+        self.time = 0
+
+    def increment(self):
+        self.time += 0.1
+        return self.time
+
+
+@pytest.fixture
+def time_mock(mocker):
+    timer = Timer()
+    mock = mocker.patch("clickshot.retry.time")
+    mock.monotonic.side_effect = timer.increment
+    return timer
+
+
 class TestRetry:
     def test_returns_if_no_exception(self, mocker):
         method = mocker.Mock(return_value=42)
@@ -20,26 +37,15 @@ class TestRetry:
 
         assert_that(result, is_(42))
 
-    def test_raises_if_exception_after_timeout(self, mocker):
-        time = mocker.patch("clickshot.retry.time")
-        method_timing = [
-            (0, NormalTestError),
-            (11, NormalTestError),
-            (21, NormalTestError),
-            (29, NormalTestError),
-            (31, NormalTestError),
-            (41, NormalTestError),
-            (51, NormalTestError),
-        ]
-        time.monotonic.side_effect = [0] + [s[0] for s in method_timing]
-        method = mocker.Mock(side_effect=[s[1] for s in method_timing])
+    def test_raises_if_exception_after_timeout(self, mocker, time_mock):
+        method = mocker.Mock(side_effect=NormalTestError)
 
         with pytest.raises(NormalTestError):
             retry_with_timeout(method, 30)
 
-    def test_raises_immediately_if_exception_and_no_timeout_set(self, mocker):
-        time = mocker.patch("clickshot.retry.time")
-        time.monotonic.return_value = 1000
+    def test_raises_immediately_if_exception_and_no_timeout_set(
+        self, mocker, time_mock
+    ):
         method = mocker.Mock(side_effect=NormalTestError)
 
         with pytest.raises(NormalTestError):
@@ -47,54 +53,33 @@ class TestRetry:
 
         method.assert_called_once()
 
-    def test_returns_if_no_exception_just_before_timeout(self, mocker):
-        time = mocker.patch("clickshot.retry.time")
-        method_timing = [
-            (0, NormalTestError),
-            (11, NormalTestError),
-            (21, NormalTestError),
-            (29, 42),
-            (31, 43),
-            (41, 44),
-            (51, 45),
-        ]
-        time.monotonic.side_effect = [0] + [s[0] for s in method_timing]
-        method = mocker.Mock(side_effect=[s[1] for s in method_timing])
+    def test_returns_if_no_exception_just_before_timeout(self, mocker, time_mock):
+        def side_effect():
+            if time_mock.time < 30:
+                raise NormalTestError
+            return 42
+
+        method = mocker.Mock(side_effect=side_effect)
 
         result = retry_with_timeout(method, 30)
 
         assert_that(result, is_(42))
 
-    def test_raises_if_abort_is_raised(self, mocker):
-        time = mocker.patch("clickshot.retry.time")
-        method_timing = [
-            (0, NormalTestError),
-            (11, NormalTestError),
-            (21, RetryAbort(AbortedTestError())),
-            (29, 42),
-            (31, 43),
-            (41, 44),
-            (51, 45),
-        ]
-        time.monotonic.side_effect = [0] + [s[0] for s in method_timing]
-        method = mocker.Mock(side_effect=[s[1] for s in method_timing])
+    def test_raises_if_abort_is_raised(self, mocker, time_mock):
+        def side_effect():
+            if time_mock.time < 10:
+                raise NormalTestError
+            elif time_mock.time < 11:
+                raise RetryAbort(AbortedTestError())
+            return 42
+
+        method = mocker.Mock(side_effect=side_effect)
 
         with pytest.raises(AbortedTestError):
             retry_with_timeout(method, 30)
 
-    def test_emits_single_warning_if_file_not_found(self, mocker):
-        time = mocker.patch("clickshot.retry.time")
-        method_timing = [
-            (0, FileNotFoundError("error message")),
-            (11, FileNotFoundError("error message")),
-            (21, FileNotFoundError("error message")),
-            (29, FileNotFoundError("error message")),
-            (31, FileNotFoundError("error message")),
-            (41, FileNotFoundError("error message")),
-            (51, FileNotFoundError("error message")),
-        ]
-        time.monotonic.side_effect = [0] + [s[0] for s in method_timing]
-        method = mocker.Mock(side_effect=[s[1] for s in method_timing])
+    def test_emits_single_warning_if_file_not_found(self, mocker, time_mock):
+        method = mocker.Mock(side_effect=FileNotFoundError("error message"))
 
         with pytest.raises(FileNotFoundError):
             with pytest.warns(UserWarning) as record:
@@ -103,19 +88,13 @@ class TestRetry:
         warning_messages = [r.message.args[0] for r in record]
         assert_that(warning_messages, is_(["error message"]))
 
-    def test_logs_progress_if_logging_enabled(self, mocker, capsys):
-        time = mocker.patch("clickshot.retry.time")
-        method_timing = [
-            (0, NormalTestError),
-            (4, 40),
-            (21, 41),
-            (29, 42),
-            (31, 43),
-            (41, 44),
-            (51, 45),
-        ]
-        time.monotonic.side_effect = [0] + [s[0] for s in method_timing]
-        method = mocker.Mock(side_effect=[s[1] for s in method_timing])
+    def test_logs_progress_if_logging_enabled(self, mocker, time_mock, capsys):
+        def side_effect():
+            if time_mock.time < 4:
+                raise NormalTestError
+            return 42
+
+        method = mocker.Mock(side_effect=side_effect)
 
         retry_with_timeout(method, 30, log=True)
 
@@ -123,19 +102,13 @@ class TestRetry:
             capsys.readouterr().out, is_("0...\b\b\b\b1...\b\b\b\b2...\b\b\b\b3...")
         )
 
-    def test_does_not_log_progress_by_default(self, mocker, capsys):
-        time = mocker.patch("clickshot.retry.time")
-        method_timing = [
-            (0, NormalTestError),
-            (4, 40),
-            (21, 41),
-            (29, 42),
-            (31, 43),
-            (41, 44),
-            (51, 45),
-        ]
-        time.monotonic.side_effect = [0] + [s[0] for s in method_timing]
-        method = mocker.Mock(side_effect=[s[1] for s in method_timing])
+    def test_does_not_log_progress_by_default(self, mocker, time_mock, capsys):
+        def side_effect():
+            if time_mock.time < 4:
+                raise NormalTestError
+            return 42
+
+        method = mocker.Mock(side_effect=side_effect)
 
         retry_with_timeout(method, 30)
 
